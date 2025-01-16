589d19b708a84b10c5a22c3351325b759b421922
e10withadot <e10withadot@gmail.com>
2025-01-16 18:34:44 +0200

    removed useless ipheader struct

--- traceroute.c
+++ traceroute.c
@@ -12,22 +12,6 @@
 #include <getopt.h>
 #include "config.h"
 
-struct ipheader
-{
-    unsigned char iph_ihl : 5,       // IP header length
-        iph_ver : 4;                 // IP version
-    unsigned char iph_tos;           // Type of service
-    unsigned short int iph_len;      // IP Packet length (data + header)
-    unsigned short int iph_ident;    // Identification
-    unsigned short int iph_flag : 3, // Fragmentation flags
-        iph_offset : 13;             // Flags offset
-    unsigned char iph_ttl;           // Time to Live
-    unsigned char iph_protocol : 1;  // Protocol type
-    unsigned short int iph_chksum;   // IP datagram checksum
-    struct sockaddr iph_sourceip;    // Source IP address
-    struct in_addr iph_destip;       // Destination IP address
-};
-
 int main(int argc, char *argv[])
 {
     int opt;
